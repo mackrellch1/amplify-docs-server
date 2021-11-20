b926b190c3cce355eaddbfb0664f550526e9799c
mackrellch1 <32115012+mackrellch1@users.noreply.github.com>
2021-11-20 15:28:28 -0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,38 @@
+require("dotenv").config();
+const mongoose = require('mongoose')
+const Document = require('./Document')
+
+mongoose.connect(process.env.MONGO_URI)
+
+const io = require('socket.io')(3002, {
+    cors: {
+        origin: 'http://localhost:3000',
+        methods: '*'
+    }
+})
+
+const defaultValue = ""
+
+io.on("connection", socket => {
+    socket.on('get-document', async documentId => {
+        const document = await findOrCreateDocument(documentId)
+        socket.join(documentId)
+        socket.emit('load-document', document.data)
+
+        socket.on('send-changes', delta => {
+            socket.broadcast.to(documentId).emit("receive-changes", delta)
+        })
+
+        socket.on('save-document', async data => {
+            await Document.findByIdAndUpdate(documentId, { data })
+        })
+    })
+})
+
+async function findOrCreateDocument(id) {
+    if (!id) return
+
+    const document = await Document.findById(id)
+    if (document) return document
+    return await Document.create({_id: id, data: defaultValue})
+}
